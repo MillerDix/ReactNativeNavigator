30e6656e9abd437e20182a786ca320cb2f0eae6a
MillerDix <zlw2579@gmail.com>
2016-06-07 15:52:03 +0800

    add logo performance

--- src/app.js
+++ src/app.js
@@ -6,14 +6,23 @@ import {
     View,
     Text,
     Alert,
+    Image,
     Navigator
 } from 'react-native';
 
 import FirstPage from './FirstPage';
 
 class NavigationTest extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            logo: '1'
+        }
+    }
     clickLogo() {
-        Alert.alert('Logo');
+        this.setState({
+            logo: '2'
+        });
     }
 
     renderNavBar() {
@@ -80,24 +89,57 @@ class NavigationTest extends Component {
     }
 
     render() {
-        let defaultName = 'FirstPage';
-        let defaultComponent = FirstPage;
-        return (
-            <Navigator
-                initialRoute = {{ name: defaultName, component: defaultComponent }}
-                // configureScene={(route) => {
-                //     return Navigator.SceneConfigs.VerticalDownSwipeJump;
-                // }}
-                renderScene={(route, navigator) => {
-                    let Component = route.component;
-                    if(route.component) {
-                        return <Component {...route.params} navigator={navigator} />
-                    }
-                }}
-                navigationBar={this.renderNavBar()}
-            />
-        );
+        if(this.state.logo == '1') {
+            let defaultName = 'FirstPage';
+            let defaultComponent = FirstPage;
+            return (
+                <Navigator
+                    initialRoute = {{ name: defaultName, component: defaultComponent }}
+                    // configureScene={(route) => {
+                    //     return Navigator.SceneConfigs.VerticalDownSwipeJump;
+                    // }}
+                    renderScene={(route, navigator) => {
+                        let Component = route.component;
+                        if(route.component) {
+                            return <Component {...route.params} navigator={navigator} />
+                        }
+                    }}
+                    navigationBar={this.renderNavBar()}
+                />
+            );
+        } else {
+            return (
+                <View style={styles.container} >
+                    <Image source={require('../images/motocycle.png')} />
+                    <TouchableOpacity style={styles.button} onPress={() => this.setState({logo: '1'})} >
+                        <Text style={styles.text}>Go Back</Text>
+                    </TouchableOpacity>
+                </View>
+            );
+        }
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 2,
+        borderColor: 'darkcyan',
+        marginTop: 5
+    },
+    text: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'darkcyan',
+        fontSize: 25,
+        fontWeight: '400',
+    }
+});
 
 AppRegistry.registerComponent('NavigationTest', () => NavigationTest);
